d04215c59ce13ff222ecd60a8c2578f0ab40b20c
jessicanetto <75263227+jessicanetto@users.noreply.github.com>
2021-02-28 13:52:11 -0500

    test passing

--- index.js
+++ index.js
@@ -1,21 +1,67 @@
-//put dice in array
+//put dice in array to pull from
 let dice = [
   {
-    "&#9856": "1",
+    di: "&#9856",
+    value: 1,
   },
   {
-    "&#9857": "2",
+    di: "&#9857",
+    value: 2,
   },
   {
-    "&#9858": "3",
+    di: "&#9858",
+    value: 3,
   },
   {
-    "&#9859": "4",
+    di: "&#9859",
+    value: 4,
   },
   {
-    "&#9860": "5",
+    di: "&#9860",
+    value: 5,
   },
   {
-    "&#9861": "6",
+    di: "&#9861",
+    value: 6,
   },
 ];
+//grab from html
+let rollTheDiceButton = document.getElementById("roll-button");
+let enteredDiceNum = document.getElementById("enter-dice-num");
+
+//cypress looking for hashtag paras
+const diceElement = document.querySelector("#dice-para");
+const sumStorage = document.querySelector("#sum-para");
+
+//list
+const ul = document.createElement("ul");
+document.body.appendChild(ul);
+
+//execute function onclick
+rollTheDiceButton.addEventListener("click", () => {
+  rollTheDice();
+});
+
+//make function with a loop to randomize rolls and store cumulative value
+function rollTheDice() {
+  diceElement.innerHTML = "";
+  let sum = 0;
+  //no need to roll multiple dice, same result as rolling one multiple times
+  //loop runs as many times as number of dice to simulate this
+  for (let i = 0; i < enteredDiceNum.value; i++) {
+    let generateRoll = Math.floor(Math.random() * 6);
+
+    diceElement.innerHTML += dice[generateRoll].di;
+    sum += dice[generateRoll].value;
+  }
+  sumStorage.textContent = `The sum of this roll is = ${sum}`;
+  let rollCount = 0;
+  rollCount++;
+
+  //history
+  if (rollCount > 0) {
+    const list = document.createElement("li");
+    ul.appendChild(list);
+    list.textContent = `${diceElement.innerHTML} ${sumStorage.textContent}`;
+  }
+}
